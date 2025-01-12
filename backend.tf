2354154d05241c5d610fa57a43a2b13065f1e0c7
UncleJay22 <johnanyanwu1759@outlook.com>
2025-01-12 04:11:55 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "my-s3-bucket-for-tf-state"
     key = "main"
     region = "eu-west-2"
-    dynamodb_table = "my-dynamo-db-table-tf"
+    dynamodb_table = "my-dynamodb-table-dec-25"
   }
 }
